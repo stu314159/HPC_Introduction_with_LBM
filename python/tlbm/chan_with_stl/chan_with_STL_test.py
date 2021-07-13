cbba81460513166b4814f3028807020be9b5c234
Stu Blair <stu314159@gmail.com>
2021-07-13 09:53:21 -0500

    added STL files for FSAE project

--- python/tlbm/chan_with_stl/chan_with_STL_test.py
+++ python/tlbm/chan_with_stl/chan_with_STL_test.py
@@ -11,13 +11,13 @@ sys.path.insert(1,'.')
 
 import FluidChannel as fc
 
-aLx_p = 4; aLy_p = 4; aLz_p = 9.0;
+aLx_p = 200; aLy_p = 200; aLz_p = 100;
 
-stl_filename = 'HalfDonut_moved.stl';
+stl_filename = 'wind_turbine_moved.stl';
 
 myObst = fc.STL_Obstruction(stl_filename);
 
 myChan = fc.FluidChannel(Lx_p=aLx_p,Ly_p=aLy_p,Lz_p=aLz_p,
-                         obst=myObst,N_divs=50);
+                         obst=myObst,N_divs=231);
 
 myChan.write_bc_vtk();
